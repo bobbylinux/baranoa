02897fd7e536836835106d3242c2c3d986341395
roberto <roberto.bani@gmail.com>
2017-10-04 18:28:24 +0200

    corretto nome file migration CreateAccessesTerapiesTable -> CreateAccessesTherapiesTable

--- database/migrations/2017_09_15_232029_create_accesses_therapies_table.php
+++ database/migrations/2017_09_15_232029_create_accesses_therapies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAccessesTerapiesTable extends Migration
+class CreateAccessesTherapiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateAccessesTerapiesTable extends Migration
      */
     public function up()
     {
-        Schema::create('accesses_terapies', function (Blueprint $table) {
+        Schema::create('accesses_therapies', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('access_id');
-            $table->integer('terapy_id');
+            $table->integer('therapy_id');
             $table->timestamps();
             /*foreign keys*/
             $table->foreign('access_id')->references('id')->on('accesses');
-            $table->foreign('terapy_id')->references('id')->on('terapies');
+            $table->foreign('therapy_id')->references('id')->on('therapies');
         });
     }
 
